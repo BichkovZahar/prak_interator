c4cfea2a1edbf9140dc35f87dfa30ab2aba9dc8f
student <Bichk_di78@student.itstep.org>
2023-05-18 20:50:50 +0300

    4

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-class OddIterator:
-   def __init__(self, n):
-       if not isinstance(n, int) or n <= 0:
-           raise ValueError("треба n ціле натуральне число")
-       self.n = n
-       self.current = 1
-   def __iter__(self):
-        return self
-   def __next__(self):
-        if self.current > self.n:
-            raise StopIteration
-        else:
-            result = self.current
-            self.current +=2
-            return result
-try:
-    for num in OddIterator([1,2,3]):
-        print(num)
-except ValueError as e:
-    print (e)
+def multiply_by():
+    def mult(a , b):
+        return a * b
+    return mult
+mult = multiply_by()
+print(mult(3 , 9))
